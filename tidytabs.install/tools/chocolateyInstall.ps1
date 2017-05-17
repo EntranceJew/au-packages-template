c5e33f74fc4d82e7482a9b7384ac29cc53d178f9
EntranceJew <EntranceJew@gmail.com>
2017-05-17 14:03:48 -0400

    [AU tidytabs tidytabs.install] added tidytabs

---
+++ tidytabs.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url32       = 'http://www.nurgo-software.com/download/TidyTabs.msi'
+$checksum32  = '58423eccfaecce547934e3a206e6da39e3d9245b6cd17134be766ac7e1c030f1'
+
+$packageArgs = @{
+  packageName   = 'tidytabs.install'
+  fileType      = 'MSI'
+  softwareName  = 'TidyTabs*'
+  
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  url           = $url32
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
