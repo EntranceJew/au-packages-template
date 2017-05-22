5950679c5aa25b14b0c5a132379d89529e8a55cc
EntranceJew <EntranceJew@gmail.com>
2017-05-22 19:15:45 -0400

    fixed up 010editor.portable to not use admin permissions

--- 010editor.portable/tools/chocolateyInstall.ps1
+++ 010editor.portable/tools/chocolateyInstall.ps1
@@ -4,12 +4,27 @@ $url32       = 'https://www.sweetscape.com/download/010EditorWin32Portable.exe'
 $url64       = 'https://www.sweetscape.com/download/010EditorWin64Portable.exe'
 $checksum32  = '28ba4beecb8e4e5d87c5bd2b4986a25341734956c1bc47034e80241618ee84d7'
 $checksum64  = 'ccf8f90615d7de64734aa3e3974e3e9ec40e9092bd84cadbf0b6a139706e2cb4'
-$installLocation = Join-Path $(Get-ToolsLocation) '010editor'
+$installLocation = Join-Path "$env:ChocolateyInstall\lib" "010editor.portable\tools"
+
+$cwd = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+if((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne $true) {
+	$unzip_file = Split-Path $url64 -leaf
+} else {
+	$unzip_file = Split-Path $url32 -leaf
+}
+
+New-Item "$installLocation\010EditorPortable.exe.gui" -type file -force | Out-Null
+New-Item "$installLocation\AppData\assistant.exe.ignore" -type file -force | Out-Null
+New-Item "$installLocation\AppData\010Editor.exe.ignore" -type file -force | Out-Null
+
+$full_dl_filename = (Join-Path "$cwd" "$unzip_file")
 
 $packageArgs = @{
   packageName   = '010editor.portable'
   fileType      = 'EXE'
   softwareName  = '010 Editor*'
+  FileFullPath  = $full_dl_filename
   
   checksum      = $checksum32
   checksumType  = 'sha256'
@@ -18,9 +33,12 @@ $packageArgs = @{
   checksum64    = $checksum64
   checksumType64= 'sha256'
   url64bit      = $url64
-
-  silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /DIR=`"$installLocation`""
-  validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Get-ChocolateyWebFile @packageArgs
+
+$extractPath = (Join-Path "$installLocation" "..\extract")
+innounp -x -b -y -d"$extractPath" "$full_dl_filename"
+robocopy (Join-Path "$extractPath" "{app}") "$installLocation" /e /mov /NFL /NDL /NJH /NJS /nc /ns /np
+Remove-Item -Path $extractPath -Recurse -Force
+Remove-Item -Path "$full_dl_filename" -Force
