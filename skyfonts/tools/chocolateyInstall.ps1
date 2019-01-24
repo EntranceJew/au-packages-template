c4d67b9dd28b8d6bbc9ab997628476a7d5fd067c
Chocolatey <chocolatey@realdimensions.net>
2019-01-24 17:40:16 +0000

    AU: 1 updated - skyfonts [skip ci] https://gist.github.com/725178eed15765f147e1d8515fff3280/6999b90caa1f820d1b0fdd481b2ec34ed3c69743

--- skyfonts/tools/chocolateyInstall.ps1
+++ skyfonts/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url32       = 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Winx86_5.9.5.0.exe.zip'
-$url64       = 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Winx64_5.9.5.0.exe.zip'
-$checksum32  = 'e0334e7ddbc94f750fc3ea99db7f619da043a036fe029cbd2cb1b2cb98808c8f'
-$checksum64  = '2a6b0a6fb2541461ba98a2bc2739d03cd01a324393d5b98773692e682dfb7c63'
+$url32       = 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Winx86_5.9.5.1.exe.zip'
+$url64       = 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Winx64_5.9.5.1.exe.zip'
+$checksum32  = 'ebc9a731bc989b360ef4756dc70692e2896f881261a918ab5f66a151515204c3'
+$checksum64  = '277f690124669f24ff114977869ad70c9873f36b784fa3efbd32a5b37a523f1d'
 
 if((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne $true) {
 	$unzip_file = Split-Path $url64 -leaf
