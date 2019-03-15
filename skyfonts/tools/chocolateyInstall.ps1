cce8558cac2dedfb8424994f86c17bc2b2b0f3af
RJ Hsiao <rj@rjhsiao.me>
2019-03-16 06:16:11 +0800

    Update skyfonts to 5.9.5.3

--- skyfonts/tools/chocolateyInstall.ps1
+++ skyfonts/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url32       = 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Winx86_5.9.5.1.exe.zip'
-$url64       = 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Winx64_5.9.5.1.exe.zip'
-$checksum32  = 'ebc9a731bc989b360ef4756dc70692e2896f881261a918ab5f66a151515204c3'
-$checksum64  = '277f690124669f24ff114977869ad70c9873f36b784fa3efbd32a5b37a523f1d'
+$url32       = 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Winx86_5.9.5.3.exe.zip'
+$url64       = 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Winx64_5.9.5.3.exe.zip'
+$checksum32  = 'a540bef469ffe6729ecf8e6c47559fbed9b6493fcff43282c6e0fd8b1570beb5'
+$checksum64  = 'bcc320dc207d4e883ba3e02b90e589cc2b721378890741bf083bf253b94d6151'
 
 if((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne $true) {
 	$unzip_file = Split-Path $url64 -leaf
